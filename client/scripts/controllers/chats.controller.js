85692dd6bb46bd33c9782646e8a7e1b53180030c
pika298 <kit2008@live.com>
2016-10-24 15:36:30 +0800

    create realtime server

--- client/scripts/controllers/chats.controller.js
+++ client/scripts/controllers/chats.controller.js
@@ -1,20 +1,20 @@
 import Moment from 'moment';
 import { Controller } from 'angular-ecmascript/module-helpers';
-import { Chat } from '../../../lib/collections';
+import { Chats } from '../../../lib/collections';
 
 export default class ChatsCtrl extends Controller {	
 	constructor() {
 		super(...arguments);
 
-		this.helpers({
-			cata() {
-				return Chats.find();
-			}
-		});
+	    this.helpers({
+	      data() {
+	        return Chats.find();
+	      }
+	    });
 	}
 
 	remove(chat) {
-		Chats.remove(chat._id);
+   		Chats.remove(chat._id);
 	}
 }
 
